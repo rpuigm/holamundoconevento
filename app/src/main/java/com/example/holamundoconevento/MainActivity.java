3510c203bf0f6a8ed26e656beb1944fb5e8447b4
rpuigm <ruben.puig.moreno@gmail.com>
2021-01-13 14:23:54 +0100

    feat: ejercicios 1 y 2

--- app/src/main/java/com/example/holamundoconevento/MainActivity.java
+++ app/src/main/java/com/example/holamundoconevento/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.holamundoconevento;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.interfaces);
+        setContentView(R.layout.ejercicio_2);
+
+        textView=(TextView)findViewById(R.id.textViewSaludo);
     }
+
+    public void saludando(View view){
+        textView.setVisibility(View.VISIBLE);
+    }
+
+
 }
