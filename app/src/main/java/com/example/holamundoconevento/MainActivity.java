aecfe90d0bfb1282d320a53dceef783646b96193
rpuigm <ruben.puig.moreno@gmail.com>
2021-01-13 17:07:29 +0100

    feat: ejercicio3

--- app/src/main/java/com/example/holamundoconevento/MainActivity.java
+++ app/src/main/java/com/example/holamundoconevento/MainActivity.java
@@ -4,22 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ejercicio_2);
 
+        button=(Button)findViewById(R.id.btnSaludar);
         textView=(TextView)findViewById(R.id.textViewSaludo);
-    }
 
-    public void saludando(View view){
-        textView.setVisibility(View.VISIBLE);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(R.string.testoSaludo);
+                textView.setTextSize(20);
+                textView.setTextColor(getResources().getColor(R.color.design_default_color_primary));
+            }
+        });
     }
 
 
